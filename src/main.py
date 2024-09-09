e7ae5bea84124af12431838c46fb400b9d42cfbf
mshrynzw <iq87io25@gmail.com>
2024-09-09 09:17:44 +0900

    Add: Env

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+import os
 from common import set_log, set_common
 from logging import getLogger, config
+from dotenv import load_dotenv
 
 
 log_conf = set_log()
@@ -9,5 +11,10 @@ logger = getLogger(__name__)
 
 common_conf = set_common()
 
+load_dotenv('.env')
+
+
 if __name__ == '__main__':
     logger.info(common_conf['AAA'])
+    API_KEY = os.getenv('YOUTUBE_API_KEY')
+    logger.info(API_KEY)
