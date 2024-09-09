1d37b5a918b574f5dbcf9fd2e73e1c624aa0bbab
mshrynzw <iq87io25@gmail.com>
2024-09-09 09:45:21 +0900

    Add: Get YouTube Data

--- src/common.py
+++ src/common.py
@@ -20,3 +20,58 @@ def set_log():
     log_conf["handlers"]["fileHandler"]["filename"] = './log/{}.log'.format(datetime.utcnow().strftime("%Y%m%d%H%M%S"))
 
     return log_conf
+
+
+def get_channel_videos(youtube, channel_id, total_results=100):
+    all_videos = []
+    next_page_token = None
+
+    while len(all_videos) < total_results:
+        search_response = youtube.search().list(
+            channelId=channel_id,
+            type='video',
+            part='id,snippet',
+            order='date',
+            maxResults=50,
+            pageToken=next_page_token
+        ).execute()
+
+        all_videos.extend(search_response['items'])
+
+        next_page_token = search_response.get('nextPageToken')
+
+        if not next_page_token:
+            break
+
+    return all_videos[:total_results]
+
+
+def get_video_details(logger, youtube, video_id):
+    video_response = youtube.videos().list(
+        part='snippet,statistics',
+        id=video_id
+    ).execute()
+
+    video_info = video_response['items'][0]
+    title = video_info['snippet']['title']
+    published_at = datetime.strptime(video_info['snippet']['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
+    view_count = int(video_info['statistics'].get('viewCount', 0))
+    comment_count = int(video_info['statistics'].get('commentCount', 0))
+    like_count = int(video_info['statistics'].get('likeCount', 0))
+
+    logger.info(f'id: {video_id}')
+    logger.info(f'title: {title}')
+    logger.info(f'published: {published_at.strftime("%Y-%m-%d %H:%M:%S")}')
+    logger.info(f'views: {view_count}')
+    logger.info(f'comments: {comment_count}')
+    logger.info(f'likes: {like_count}')
+    logger.info('---')
+
+    return {
+        'id': video_id,
+        'title': title,
+        'published_at': published_at,
+        'view_count': view_count,
+        'comment_count': comment_count,
+        'like_count': like_count
+    }
--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 import os
-from common import set_log, set_common
+import logging
+from common import set_log, set_common, get_channel_videos, get_video_details
 from logging import getLogger, config
 from dotenv import load_dotenv
+from googleapiclient.discovery import build
 
+# ログ設定を適用
+logging.config.dictConfig(set_log())
 
-log_conf = set_log()
-config.dictConfig(log_conf)
-logger = getLogger(__name__)
+# メインのロガーを取得
+logger = logging.getLogger(__name__)
 
 common_conf = set_common()
 
@@ -15,6 +18,23 @@ load_dotenv('.env')
 
 
 if __name__ == '__main__':
-    logger.info(common_conf['AAA'])
     API_KEY = os.getenv('YOUTUBE_API_KEY')
-    logger.info(API_KEY)
+
+    # YouTube APIクライアントを作成
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
+
+    # チャンネルIDを指定
+    channel_id = 'UCxBR2bnAFAavDHpHtQrTA9Q'
+    
+    # 取得したい総数
+    total_results = 100
+
+    # チャンネルの動画を取得
+    all_videos = get_channel_videos(youtube, channel_id, total_results)
+
+    # 結果を処理
+    video_details = []
+    for item in all_videos:
+        video_id = item['id']['videoId']
+        video_detail = get_video_details(logger, youtube, video_id)
+        video_details.append(video_detail)
